e862723181d182554d520035b868f3796ff35b9f
ethrydevelops <58019814+ethrydevelops@users.noreply.github.com>
2025-06-15 23:43:27 +1200

    navbar check if logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,18 @@ import { useLocation } from 'preact-iso';
 import { useState } from 'preact/hooks';
 import "../nav.css";
 import { Toaster } from 'react-hot-toast';
+import Cookies from 'universal-cookie';
 
 export function NavBar() {
 	const { url } = useLocation();
 
 	const [navVisible, setNavVisible] = useState(true);
-
+	
+	const cookies = new Cookies(null, { path: "/" });
+	const authToken = cookies.get("gronk_tk");
+	
+	const isLoggedIn = !!authToken;
+	
 	return (
 		<div className="nav-container">
 			<div className="secondary-nav-outer-top">
@@ -24,18 +30,42 @@ export function NavBar() {
 					<div className="nav-top-flex">
 						<img src="/gronk.svg" alt="" className="nav-logo" />
 						<div className="nav-icons-flex">
-							<button title="New Chat" className="nav-button nav-open-nav-button" onClick={() => alert("// TODO: implement new chat button")}>
-								<span className="material-symbols-rounded nav-icon">add</span>
-							</button>
+							{isLoggedIn ? (
+								<button title="New Chat" className="nav-button nav-open-nav-button" onClick={() => alert("// TODO: implement new chat button")}>
+									<span className="material-symbols-rounded nav-icon">add</span>
+								</button>
+							) : null}
 							
 							<button title="Toggle Sidebar" className="nav-button nav-open-nav-button" onClick={() => setNavVisible(!navVisible)}>
 								<span className="material-symbols-rounded nav-icon">thumbnail_bar</span>
 							</button>
 						</div>
 					</div>
+
+					{/* chats */}
+
+					{
+						isLoggedIn ? (
+							<div className="nav-chats-list">
+								<div className="nav-chats-list-top">
+									<form className="nav-chats-list-search-flex">
+										<input type="text" className="nav-chats-list-search-input-text" placeholder="Search" />
+
+										<button className="btn btn-primary square h-100 nav-chats-list-search-submit">
+											<span class="material-symbols-rounded">
+												search
+											</span>
+										</button>	
+									</form>
+								</div>
+
+								{/* TODO: scrolling */}
+							</div>
+						) : null
+					}
 				</nav>
 			</div>
-			
+
 			<Toaster
 				position="top-center"
 				reverseOrder={false}
