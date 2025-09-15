e00002aae7f3ff586143aaaf02fa8811b1f12318
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2025-09-16 00:26:04 +1200

    init server

---
+++ server/migrations/20250914015600_users.js
@@ -0,0 +1,48 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("users", (table) => {
+        table.uuid("uuid").primary();
+        table.string("username").notNullable().unique();
+        table.string("email").notNullable().unique();
+        table.string("password_hash").notNullable();
+        table.timestamps(true, true);
+    })
+    .createTable("sessions", (table) => {
+        table.uuid("uuid").primary();
+        table.uuid("user_uuid").notNullable().references("uuid").inTable("users").onDelete("CASCADE");
+        table.string("token_hash").notNullable().unique();
+        table.timestamps(true, true);
+    })
+    .createTable("models", (table) => {
+        table.uuid("uuid").primary();
+        
+        table.string("name").notNullable();
+        table.string("description");
+
+        table.uuid("owner_uuid").nullable().references("uuid").inTable("users").onDelete("CASCADE");
+
+        // default instance models should have both set to true, but users' models should have both false:
+        table.boolean("is_public").defaultTo(false); // if instance provides model for free
+        table.boolean("conn_private").defaultTo(false); // if connection info is not shared with user(s) (i.e, API key, url)
+
+        table.string("api_url").notNullable();
+        table.string("api_authorization");
+        table.string("api_model"); // e.g., "gpt-4o", "gpt-3.5-turbo", "claude-2"
+
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists("sessions")
+        .dropTableIfExists("users")
+        .dropTableIfExists("models");
+};
