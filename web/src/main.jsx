26da3ce4646869ee0f8acd23bbff77501efa970b
ethrydevelops <58019814+ethrydevelops@users.noreply.github.com>
2025-09-21 20:43:26 +1200

    web: remove clerk envs

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -12,12 +12,6 @@ import App from './App.jsx'
 import AuthPage from './AuthPage.jsx';
 import NavBar from './components/NavBar.jsx';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-    throw new Error('Add your Clerk Publishable Key to the .env file')
-}
-
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <div className="app-container">
