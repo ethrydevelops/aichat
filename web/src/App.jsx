454b95a7de1b0a2513ea9179f9088ae616b22c27
ethrythedev <58019814+ethrythedev@users.noreply.github.com>
2025-10-03 23:38:25 +1300

    web: model selector (finally)

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,41 @@
-import { useEffect } from 'react';
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import ModelSelectDialog from './components/ModelSelector';
 import Cookies from 'universal-cookie'
 
 function App() {
     const cookies = new Cookies();
 
     const [prompt, setPrompt] = useState("");
+    const [model, setModel] = useState(() => {
+        try {
+            const stored = cookies.get("askllm_favourite_model"); 
+
+            if (stored) {
+                return typeof stored === "object" ? stored : JSON.parse(stored);
+            }
+        } catch (e) {
+            console.error("Invalid JSON in cookie:", e);
+        }
+
+        return { name: "Select a model", uuid: 0 };
+    });
+
+    const [modelSelectorDialogOpen, setModelSelectorDialogOpen] = useState(false);
     const [username, setUsername] = useState(cookies.get('askllm_usernameoverride') || "");
+
+    function handleSendMessage(ev) {
+        ev.preventDefault();
+
+        if(prompt.trim() === "") return;
+
+        alert("You asked: " + prompt);
+        setPrompt("");
+    }
+
+    function modelPopup(ev) {
+        ev.preventDefault();
+        setModelSelectorDialogOpen(!modelSelectorDialogOpen);
+    }
 
     useEffect(() => {
         // get username from account if logged in
@@ -24,17 +53,38 @@ function App() {
                 }
             });
         }
-    }, [cookies]);
+    }, []);
 
     return (
         <div className="d-flex-middle">
             <div>
                 <h1 className="landing-page-title">How can I help you{username ? (", " + username) : ""}?</h1>
-                <form action="/" className="landing-page-prompt-box-outer">
-                    <textarea name="prompt" id="prompt" placeholder="Ask anything" className="landing-page-prompt-box" rows={1} value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
-                    <button type="submit" className="landing-page-prompt-box-btn-ol">
-                        <span className="material-symbols-rounded">arrow_forward</span>
-                    </button>
+                <form action="/">
+                    <div className="landing-page-prompt-box-outer">
+                        <textarea name="prompt" id="prompt" placeholder="Ask anything" className="landing-page-prompt-box" rows={1} value={prompt} onChange={(e) => setPrompt(e.target.value)} onKeyDown={(e) => {if (e.key === "Enter" && !e.shiftKey) {handleSendMessage(e);}}}></textarea>
+                        <button type="submit" className="landing-page-prompt-box-btn-ol" onClick={handleSendMessage} aria-label="Send Message">
+                            <span className="material-symbols-rounded">arrow_forward</span>
+                        </button>
+                    </div>
+                    <div className="landing-page-prompt-under-box">
+                        <div>
+                            <button className="landing-page-prompt-model-select-btn" onClick={modelPopup}>
+                                <span className="landing-page-prompt-model-select-btn-selected-name">
+                                    {model.name}
+                                </span>
+                                <span className="material-symbols-rounded">expand_more</span>
+                            </button>
+                            <div className="landing-page-model-select-container">
+                                <ModelSelectDialog modelSelectorDialogOpen={modelSelectorDialogOpen} activeModel={model} onModelChange={async (newModel) => {
+                                    if(!newModel) return setModelSelectorDialogOpen(false);
+                                    setModel(newModel);
+
+                                    cookies.set("askllm_favourite_model", JSON.stringify({ name: newModel.name, uuid: newModel.uuid }), { path: '/' });
+                                    setModelSelectorDialogOpen(false);
+                                }} />
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
---
+++ web/src/components/ModelSelector.jsx
@@ -0,0 +1,160 @@
+import { useEffect, useRef, useState } from "react";
+import Cookies from "universal-cookie";
+import { Link } from "react-router";
+import './ModelSelector.css';
+
+function ModelSelectDialog({ modelSelectorDialogOpen, onModelChange, activeModel }) {
+    const cookies = new Cookies();
+
+    const dialogRef = useRef(null);
+    const [models, setModels] = useState([
+        { id: 'loading', name: 'Loading', enabled: false }
+    ]);
+
+    const [lastModelScan, setLastModelScan] = useState(0);
+    const [modelsLoadError, setModelsLoadError] = useState({ show: false, message: "" });
+    const [modelsLoading, setModelsLoading] = useState(true);
+
+    useEffect(() => {
+        if (modelSelectorDialogOpen && dialogRef.current) {
+            dialogRef.current.focus();
+        }
+        
+        // if error state, reset on reopen & retry loading immediately
+        if(modelSelectorDialogOpen && modelsLoadError.show) {
+            setModelsLoadError({ show: false, message: "" });
+            setModelsLoading(true);
+            setModels([]);
+            setLastModelScan(0);
+        }
+
+        // fetch models from the api every time reopened
+        // 1.5 second debounce
+        if(Date.now() - lastModelScan > 1500) {            
+            fetch((import.meta.env.VITE_API_URL.replace(/\/$/, "")) + "/models", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${cookies.get("askllm_tk")}`
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.models) {
+                    setModelsLoading(false);
+                    setModels(data.models);
+                    setModelsLoadError({ show: false, message: "" });
+
+                    // if active model doesn't exist in list, clear or correct it
+                    if(activeModel) {
+                        let currentModelAttempt = data.models.find(m => m.uuid === activeModel.uuid);
+                        if(!currentModelAttempt) {
+                            onModelChange({name: "Select a model", uuid: 0});
+                        } else if(activeModel.name !== currentModelAttempt.name) {
+                            onModelChange(currentModelAttempt); // correct name if changed
+                        }
+                    }
+                }
+            })
+            .catch(error => {
+                setModelsLoadError({ show: true, message: error.message });
+                setModelsLoading(false);
+                setModels([]);
+                console.error("Error fetching models:", error);
+            });
+
+            setLastModelScan(Date.now());
+        }
+    }, [modelSelectorDialogOpen]);
+
+    const handleModelChange = (selectedModel) => {
+        if (onModelChange) {
+            if(!models.find(m => m.uuid === selectedModel)) return alert("Selected model not found");
+            selectedModel = models.find(m => m.uuid === selectedModel);
+
+            onModelChange(selectedModel);
+        }
+    };
+
+    return (
+        <dialog className="model-select-dialog" ref={dialogRef} open={modelSelectorDialogOpen} tabIndex={-1}>
+            <div className="modal-select-tag no-mobile">
+                <span></span>
+            </div>
+
+            <div className="no-desktop">
+                <div className="modal-top-flex">
+                    <h3 className="m-0 p-0">Select a Model</h3>
+
+                    <button className="modal-close-btn" type="button" aria-label="Close" onClick={() => onModelChange(null)}>
+                        <span className="material-symbols-rounded">close</span>
+                    </button>
+                </div>
+            </div>
+
+            <div className="model-select-options">
+                {modelsLoading ?
+                    <>
+                        <div className="placeholder-glow model-option-btn-loader-outer">
+                            <div className="placeholder model-option-btn-loader"></div>
+                        </div>
+                        <div className="placeholder-glow model-option-btn-loader-outer">
+                            <div className="placeholder model-option-btn-loader"></div>
+                        </div>
+                        <div className="placeholder-glow model-option-btn-loader-outer">
+                            <div className="placeholder model-option-btn-loader"></div>
+                        </div>
+                        <div className="placeholder-glow model-option-btn-loader-outer">
+                            <div className="placeholder model-option-btn-loader"></div>
+                        </div>
+                    </> : (
+                        models.length > 0 ? (
+                            models.map((model) => (
+                                <button type="button" key={model.uuid} className="model-option-btn" value={model.uuid} onClick={() => handleModelChange(model.uuid)} disabled={model.enabled ? !model.enabled : false}>
+                                    <div className="model-option-circle" data-model-action-circle-active={activeModel.uuid === model.uuid}></div>
+                                    <span>
+                                        {model.name}
+                                    </span>
+                                </button>
+                            ))
+                        ) : (
+                            <section className="model-modal-no-models">
+                                <div className="model-modal-no-models-warning">
+                                    <span className="material-symbols-rounded">
+                                        sentiment_dissatisfied
+                                    </span>
+                                    {modelsLoadError.show ? (
+                                        <>
+                                            <p className="m-0 p-0">Error loading models</p>
+                                            <div className="modal-warning-hint text-danger">
+                                                <span className="material-symbols-rounded">
+                                                    error
+                                                </span>
+                                                <span>
+                                                    {modelsLoadError.message}
+                                                </span>
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <p className="m-0 p-0">No models available</p>
+                                            <div className="modal-warning-hint">
+                                                <span className="material-symbols-rounded">
+                                                    info
+                                                </span>
+                                                <span>
+                                                    <Link to="/settings" className="link-secondary">Try adding one?</Link>
+                                                </span>
+                                            </div>
+                                        </>
+                                    )}
+                                </div>
+                            </section>
+                        )
+                    )}
+            </div>
+        </dialog>
+    );
+}
+
+export default ModelSelectDialog;
